62af36121e3fc6558ab2752189c54000476ff5f0
Bogdan Petrovskiy <82778510+kefaxoo@users.noreply.github.com>
2022-08-11 20:38:20 +0300

    Update UI

--- deezer-chart/deezer-chart/ContentView.swift
+++ deezer-chart/deezer-chart/ContentView.swift
@@ -13,52 +13,70 @@ struct Table: View {
     @ObservedObject var deezerAPI = DeezerAPI()
     
     var body: some View {
-        List {
-            ForEach(deezerAPI.tracks, id: \.id) { track in
-                HStack() {
-                    VStack() {
-                        if track.position == 1 {
-                            Image(systemName: "crown.fill")
-                                .foregroundColor(.yellow)
-                        }
-                        Text("\(track.position)")
-                    }
-                        .frame(width: 20, height: 10)
-                    AsyncImage(url: URL(string: track.album.cover)) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                    } placeholder: {
-                        Color.white
-                    }
-                        .frame(width: 50, height: 50)
-                        .padding(.top, 5)
-                        .padding(.bottom, 5)
-                        .padding(.horizontal)
-                    VStack() {
-                        HStack() {
-                            Text(track.title)
-                                .bold()
-                                .font(.title3)
-                            if track.explicit_lyrics {
-                                Image(systemName: "e.square.fill")
-                                    .resizable()
-                                    .frame(width: 15, height: 15)
-                                    .foregroundColor(.red)
+        if deezerAPI.tracks.isEmpty {
+            EmptyStateView()
+        } else {
+            List {
+                ForEach(deezerAPI.tracks, id: \.id) { track in
+                    HStack() {
+                        VStack() {
+                            if track.position == 1 {
+                                Image(systemName: "crown.fill")
+                                    .foregroundColor(.yellow)
                             }
-                            Spacer()
+                            Text("\(track.position)")
                         }
-                        HStack() {
-                            Text("\(track.artist.name) | \(track.album.title)")
-                                .font(.caption)
-                            Spacer()
+                            .frame(width: 20, height: 10)
+                        AsyncImage(url: URL(string: track.album.cover)) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                        } placeholder: {
+                            Color.white
                         }
+                            .frame(width: 50, height: 50)
+                            .padding(.top, 5)
+                            .padding(.bottom, 5)
+                            .padding(.horizontal)
+                            .shadow(radius: 5)
+                        VStack() {
+                            HStack() {
+                                Text(track.title)
+                                    .bold()
+                                    .font(.title3)
+                                if track.explicit_lyrics {
+                                    Image(systemName: "e.square.fill")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
+                                        .foregroundColor(.red)
+                                }
+                                Spacer()
+                            }
+                            HStack() {
+                                Text("\(track.artist.name) | \(track.album.title)")
+                                    .font(.caption)
+                                Spacer()
+                            }
+                        }
+                        Spacer()
+                        Text("\(track.duration >= 3600 ? track.duration.hh_mm_ss : track.duration.mm_ss)")
                     }
-                    Spacer()
-                    Text("\(track.duration >= 3600 ? track.duration.hh_mm_ss : track.duration.mm_ss)")
                 }
             }
+                .listStyle(PlainListStyle())
         }
+    }
+}
+
+struct EmptyStateView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Loading...")
+                .font(.title)
+            Spacer()
+        }
+            .padding()
     }
 }
 
